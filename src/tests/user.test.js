541d9039b39c15e2a77f5627b82c7f2e4626ec8e
sharan chitra <chitrasharan2000@gmail.com>
2026-09-06 09:57:14 +0530

    updated import in main test file

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../server');
+const app = require('../server');
 const sequelize = require('../config/database');
 const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals');
 
