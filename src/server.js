66d3d423101d28bb5d7da8632fa1e3c24942e203
sharan chitra <chitrasharan2000@gmail.com>
2026-09-12 14:54:56 +0530

    testing env variables

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const sequelize = require('./config/database');
 const userRoutes = require('./routes/userRoutes');
 
 // function hello() {}
+console.log(`__________________${process.env.START_MESSAGE}__________________`)
+console.log(`__________________${process.env.ENV_START_MESSAGE}__________________`)
 
 const app = express();
 app.use(express.json());
