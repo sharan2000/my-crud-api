9f0f417d3f5f84e15a239b0bdc619cf8a4a3e668
sharan chitra <chitrasharan2000@gmail.com>
2026-09-13 13:28:43 +0530

    .

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const userRoutes = require('./routes/userRoutes');
 
 // function hello() {}
 // console.log(`__________________${process.env.START_MESSAGE}__________________`)
-console.log(`__________________${process.env.ENV_START_MESSAGE}__________________`)
+// console.log(`__________________${process.env.ENV_START_MESSAGE}__________________`)
 console.log(`__________________${process.env.ENV_SECRET_MESSAGE}__________________`)
 
 const app = express();
